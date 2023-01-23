6137e93e38d8b2ef7c22dc0604e8ba1ef37f67f9
jerrymusaga <musagatechnology@gmail.com>
2023-01-23 09:18:28 +0100

    generating keypair from terminal

--- programs/sol-gift/src/lib.rs
+++ programs/sol-gift/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("BBocCx2HTUWjcsm9REjSJ3FYyE8RD5968KBiZ58ordJe");
 
 #[program]
 pub mod sol_gift {
